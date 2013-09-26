ee926c2d889a3b05aa29a531ab553be57c909347
vkorukanti <venki.korukanti@gmail.com>
2013-09-25 19:43:47 -0700

    MapR 11281 (contd.): Drop database cascade fails if any tables have indexes created as separate tables

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStoreClient.java
@@ -531,7 +531,7 @@ public class HiveMetaStoreClient implements IMetaStoreClient {
           // already deleted
         }
 
-        if (table != null)
+        if (table != null && !isIndexTable(table))
           dropTable(table, deleteData, false, null);
       }
     }
