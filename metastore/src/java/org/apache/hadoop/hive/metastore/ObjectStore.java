43a5f781a3527e3d0774fd032ddc1252c891f8cf
Na Yang <nyang@maprtec.com>
2014-11-20 13:11:17 -0800

    MAPR-15833: Hive metatool fails: HiveMetaTool:A valid host is required in both old-loc and new-loc
    Signed-off-by: Na Yang <nyang@maprtec.com>

--- metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -5508,12 +5508,15 @@ public class ObjectStore implements RawStore, Configurable {
     }
     //compare hosts
     if (onDiskHost != null) {
-      if (!inputHost.equalsIgnoreCase(onDiskHost)) {
+      if (inputHost == null || !inputHost.equalsIgnoreCase(onDiskHost)) {
         return false;
       }
     } else {
-      return false;
+      if (inputHost != null) {
+        return false;
+      }
     }
+
     return true;
   }
 
