559af0c5df07ff15af6487b5379fb6cc01d15f9d
Na Yang <nyang@maprtec.com>
2015-01-14 14:56:45 -0800

    MAPR-16727: WebHcat fails to start on Yarn cluster

--- hcatalog/webhcat/svr/src/main/bin/webhcat_config.sh
+++ hcatalog/webhcat/svr/src/main/bin/webhcat_config.sh
@@ -121,7 +121,6 @@ fi
 #====================================
 #check HADOOP_HOME and then check HADOOP_PREFIX
 if [ -f ${HADOOP_HOME}/bin/hadoop ]; then
-  export HADOOP_PREFIX=$HADOOP_HOME
   export HADOOP_CONF_DIR=${HADOOP_HOME}/conf
 else
   echo "${this}: Hadoop not found."
@@ -129,6 +128,17 @@ else
 fi
 
 export BASEMAPR=${MAPR_HOME:-/opt/mapr}
+
+#get the yarn version
+if [ -f $BASEMAPR/conf/hadoop_version ];then
+   MAPR_YARN_VERSION=`cat $BASEMAPR/conf/hadoop_version | grep yarn_version | awk -F'=' '{print $2}'`
+   HADOOP_PREFIX=$BASEMAPR/hadoop/hadoop-$MAPR_YARN_VERSION
+else
+   HADOOP_PREFIX=$HADOOP_PREFIX
+fi
+
+export HADOOP_PREFIX
+
 env=${BASEMAPR}/conf/env.sh
 [ -f $env ] && . $env
 export HADOOP_OPTS="${HADOOP_OPTS} ${MAPR_ECOSYSTEM_LOGIN_OPTS}"
