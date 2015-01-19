4441453f6782a042303497981b7ef743ee4dac60
Na Yang <nyang@maprtec.com>
2015-01-19 11:34:03 -0800

    MAPR-16784: Secure Hive metastore fails to start on Mapr 4.0.2 cluster

--- shims/src/common/java/org/apache/hadoop/hive/shims/ShimLoader.java
+++ shims/src/common/java/org/apache/hadoop/hive/shims/ShimLoader.java
@@ -88,7 +88,7 @@ public abstract class ShimLoader {
     HADOOP_THRIFT_AUTH_BRIDGE_CLASSES.put("0.20S",
         "org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge20S");
     HADOOP_THRIFT_AUTH_BRIDGE_CLASSES.put("0.20SUnified",
-        "org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge20S");
+        "org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge23");
     HADOOP_THRIFT_AUTH_BRIDGE_CLASSES.put("0.23",
         "org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge23");
   }
