c1175423e472933e806fc2c74b88528d7ab6d3cb
Alina Abramova <aabramova@maprtech.com>
2016-06-06 16:58:04 +0300

    MAPR-23560 - HS2 and HiveMetaStore PID under /opt/mapr/pid

--- bin/ext/metastore.sh
+++ bin/ext/metastore.sh
@@ -62,6 +62,7 @@ pid=$HIVE_PID_DIR/hive-$HIVE_IDENT_STRING-metastore.pid
   export HADOOP_OPTS="$HIVE_METASTORE_HADOOP_OPTS $HADOOP_OPTS"
   export HADOOP_OPTS="$HADOOP_OPTS ${MAPR_HIVE_SERVER_LOGIN_OPTS}"
 
+  ln -sf $pid ${BASEMAPR}/pid
   nohup $HADOOP jar $JAR $CLASS "$@" >> "$log" 2>&1 < /dev/null &
   echo $! > $pid
   echo "`date` metastore started, pid `cat $pid`" >> "$log" 2>&1 < /dev/null
@@ -97,3 +98,4 @@ pid=$HIVE_PID_DIR/hive-$HIVE_IDENT_STRING-metastore.pid
   echo metastore not running.
   exit 1
 }
+
