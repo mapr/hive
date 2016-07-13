b77136e32abc7f2e4c8b3e6ad0abcbc13a63d449
Oleksiy Sayankin <osayankin@maprtech.com>
2016-07-13 13:08:58 +0300

    MAPR-23970 Hive CTAS does not work with ECO-1605 Hive 1.2 package

--- ql/src/java/org/apache/hadoop/hive/ql/Context.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Context.java
@@ -71,10 +71,11 @@ public class Context {
 
   // scratch path to use for all non-local (ie. hdfs) file system tmp folders
   private Path nonLocalScratchPath;
-  private boolean fNonLocalScratchDirUsed = false;
+  private boolean isNonLocalScratchDirUsed = false;
   private String CTASTableLocation;
   private boolean isCTASQuery = false;
   private boolean isInheritPerms = false;
+  private boolean isHiveOptimizeInsertDestVolume = false;
 
   // scratch directory to use for local file system tmp folders
   private final String localScratchDir;
@@ -139,10 +140,11 @@ public class Context {
     scratchDirPermission = HiveConf.getVar(conf, HiveConf.ConfVars.SCRATCHDIRPERMISSION);
     stagingDir = HiveConf.getVar(conf, HiveConf.ConfVars.STAGINGDIR);
     isInheritPerms = HiveConf.getBoolVar(conf, HiveConf.ConfVars.HIVE_WAREHOUSE_SUBDIR_INHERIT_PERMS);
+    isHiveOptimizeInsertDestVolume = HiveConf.getBoolVar(conf, HiveConf.ConfVars.HIVE_OPTIMIZE_INSERT_DEST_VOLUME);
   }
 
   public void changeDFSScratchDir(String newScratchDir) {
-    if (fNonLocalScratchDirUsed)
+    if (isNonLocalScratchDirUsed)
       throw new RuntimeException("Configured scratchdir already in use");
 
     nonLocalScratchPath = new Path(newScratchDir + "_" + executionId);
@@ -159,8 +161,8 @@ public class Context {
   }
 
   private Path getNonLocalScratchDir() {
-    if (!fNonLocalScratchDirUsed)
-      fNonLocalScratchDirUsed = true;
+    if (!isNonLocalScratchDirUsed)
+      isNonLocalScratchDirUsed = true;
     return nonLocalScratchPath;
   }
 
@@ -300,8 +302,8 @@ public class Context {
           FileSystem fs = dirPath.getFileSystem(conf);
           dirPath = new Path(fs.makeQualified(dirPath).toString());
           FsPermission fsPermission = new FsPermission(scratchDirPermission);
-          // MAPR-23153
-          if (isCTASQuery) {
+          // MAPR-23153 & MAPR-23970
+          if (isCTASQuery && isNonLocalScratchDirUsed && isHiveOptimizeInsertDestVolume) {
             // stage 1. Create table root dir with correct permissions from fs.permissions.umask-mode
             Path CTASTablePath = new Path(CTASTableLocation);
             if (!fs.mkdirs(CTASTablePath)) {
