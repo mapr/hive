7be40d012256760bfb27bd20751771b4c8ec5541
Oleksiy Sayankin <osayankin@maprtech.com>
2017-11-08 14:45:46 +0200

    DIA-373 Stabilize PR workflow
    Signed-off-by: Oleksiy Sayankin <osayankin@maprtech.com>

--- mapr-devops/lib/hive-builder.ts
+++ mapr-devops/lib/hive-builder.ts
@@ -7,9 +7,6 @@ async function buildHive() {
         const mesosEnv = "MesosDockerFarm";
         const buildTemplateId = `buildImageCentOS6`;
         const createSSHDirCmd = 'mkdir -p /root/.ssh';
-        const copyKey = `cp /deployKeys/private-hive/git_key /root/.ssh`;
-        const changePerm = `chmod 600 /root/.ssh/git_key`;
-        const addToConfig = `echo -e "StrictHostKeyChecking no\nhost source.devops.lab\nHostName source.devops.lab\nIdentityFile /root/.ssh/git_key\nUser git" > /root/.ssh/config`;
 
         const gitURL = frameworkForNodeJSInstance.process.environmentVariableNamed(`gitURL`);
         const gitBranch = frameworkForNodeJSInstance.process.environmentVariables.hasKey(`ghprbPullId`)
@@ -26,8 +23,7 @@ async function buildHive() {
         const nodeIp = cluster.nodes.first.host;
         console.log(`IP address of launched machine : ${nodeIp}`);
         await cluster.nodes.first.executeShellCommand(createSSHDirCmd);
-        await cluster.nodes.first.executeShellCommand(copyKey);
-        await cluster.nodes.first.executeShellCommands(changePerm, addToConfig);
+        await cluster.nodes.first.addProjectDeployKey(`private-hive`);
         await cluster.nodes.first.upload(__dirname + `/../scripts/run-pr-build.sh`, `/root/`);
         const buildRunResult = await cluster.nodes.first.newSSHSession().then(sshClient => sshClient.executeCommand(`gitURL=${gitURL} gitBranch=${gitBranch} ${prValue} sh /root/run-pr-build.sh`).onProgress(p => console.log(p.stdOut || p.stdErr)));
         await cluster.nodes.first.download(`/root/mapr-hive/private-hive/target/site/surefire-report.html`, `./`);
@@ -37,6 +33,7 @@ async function buildHive() {
     catch (e) {
         if (cluster)
             cluster.destroy();
+        console.log(`PR Build Failed !!!`);
         console.log(e);
         process.exit(1);
     }
