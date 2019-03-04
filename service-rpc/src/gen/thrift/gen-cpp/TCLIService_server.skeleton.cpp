c3abff224416ec522b941a286af0d94fb6f76d0d
Artem Velikorodniy <avelikorodniy@maprtech.com>
2019-03-04 18:52:08 +0200

    MAPR-HIVE-432 : CLONE - CVE-2018-1320 vulnerability in Apache Thrift

--- service-rpc/src/gen/thrift/gen-cpp/TCLIService_server.skeleton.cpp
+++ service-rpc/src/gen/thrift/gen-cpp/TCLIService_server.skeleton.cpp
@@ -12,8 +12,6 @@ using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 
-using boost::shared_ptr;
-
 using namespace  ::apache::hive::service::rpc::thrift;
 
 class TCLIServiceHandler : virtual public TCLIServiceIf {
@@ -131,11 +129,11 @@ class TCLIServiceHandler : virtual public TCLIServiceIf {
 
 int main(int argc, char **argv) {
   int port = 9090;
-  shared_ptr<TCLIServiceHandler> handler(new TCLIServiceHandler());
-  shared_ptr<TProcessor> processor(new TCLIServiceProcessor(handler));
-  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TCLIServiceHandler> handler(new TCLIServiceHandler());
+  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new TCLIServiceProcessor(handler));
+  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
   TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
   server.serve();
