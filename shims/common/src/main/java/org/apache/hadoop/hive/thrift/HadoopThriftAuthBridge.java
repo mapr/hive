7a3035629b47e3c0d37224e2f2dd94fc19640bfe
avelikorodniy <avelikorodniy@mapr.com>
2019-03-06 18:23:35 +0200

    MAPR-HIVE-465 : Investigate error logs in Hive Metastore after implementing Thrift v0.12.0

--- shims/common/src/main/java/org/apache/hadoop/hive/thrift/HadoopThriftAuthBridge.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/thrift/HadoopThriftAuthBridge.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.hive.thrift;
 import static org.apache.hadoop.fs.CommonConfigurationKeys.HADOOP_SECURITY_AUTHENTICATION;
 
 import java.io.IOException;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.security.PrivilegedAction;
@@ -590,14 +591,9 @@ public abstract class HadoopThriftAuthBridge {
             remoteUser.set(clientUgi.getShortUserName());
             LOG.debug("Set remoteUser :" + remoteUser.get());
             return clientUgi.doAs(new PrivilegedExceptionAction<Boolean>() {
-
               @Override
-              public Boolean run() {
-                try {
-                  return wrapped.process(inProt, outProt);
-                } catch (TException te) {
-                  throw new RuntimeException(te);
-                }
+              public Boolean run() throws TException {
+                return wrapped.process(inProt, outProt);
               }
             });
           } else {
@@ -607,11 +603,16 @@ public abstract class HadoopThriftAuthBridge {
             LOG.debug("Set remoteUser :" + remoteUser.get() + ", from endUser :" + endUser);
             return wrapped.process(inProt, outProt);
           }
-        } catch (RuntimeException rte) {
-          if (rte.getCause() instanceof TException) {
-            throw (TException)rte.getCause();
+        } catch (TTransportException tte) {
+          throw new RuntimeException(tte);
+        } catch (UndeclaredThrowableException ute) {
+          if (ute.getCause() instanceof TTransportException) {
+            throw new RuntimeException(ute.getCause());
           }
-          throw rte;
+          if (ute.getCause() instanceof TException) {
+            throw (TException) ute.getCause();
+          }
+          throw ute;
         } catch (InterruptedException ie) {
           throw new RuntimeException(ie); // unexpected!
         } catch (IOException ioe) {
