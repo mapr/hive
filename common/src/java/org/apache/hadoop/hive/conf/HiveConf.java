33f31045e39b56b493ff731d59226fb1a2c686f3
Oleksiy Sayankin <osayankin@maprtech.com>
2021-03-14 12:24:58 +0200

    MAPR-HIVE-898 : Unable to connect hs2 through jdbc HiveConnection

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -4537,6 +4537,7 @@ public class HiveConf extends Configuration {
     ConfVars.SHOW_JOB_FAIL_DEBUG_INFO.varname,
     ConfVars.TASKLOG_DEBUG_TIMEOUT.varname,
     ConfVars.HIVEQUERYID.varname,
+    ConfVars.HIVE_JDBC_CLIENT_AUTHENTICATION.varname
   };
 
   /**
