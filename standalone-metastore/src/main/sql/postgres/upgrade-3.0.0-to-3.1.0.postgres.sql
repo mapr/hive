dec91650fbd84cd81a6d4da79c47d45c8311f952
Oleksii Marchenko <oleksii.marchenko@hpe.com>
2024-02-20 23:26:57 +0200

    EEP-HIVE-1486: expand datatype for PARAM_VALUE column type in PARTITION_PARAMS metastore table
    Jira header: Data too long for column 'PARAM_VALUE'
    
    Partially implementing patch for HIVE-21741, adapting for hive-3.1
    
    Note: users should execute ALTER statement to change their existing
          metastore manually after receiving patch with this commit
    
    Note: in the original patch there seems to be a hiccup
          in mssql upgrade script: for some reason it alters
          SERDE_PARAMS table, and uses nvarchar insted of varchar
          as in the schema. This is fixed here.

--- standalone-metastore/src/main/sql/postgres/upgrade-3.0.0-to-3.1.0.postgres.sql
+++ standalone-metastore/src/main/sql/postgres/upgrade-3.0.0-to-3.1.0.postgres.sql
@@ -59,6 +59,9 @@ ALTER TABLE COMPLETED_COMPACTIONS ADD CC_ERROR_MESSAGE clob;
 ALTER TABLE "COMPACTION_QUEUE" ADD "CQ_ENQUEUE_TIME" bigint;
 ALTER TABLE "COMPLETED_COMPACTIONS" ADD "CC_ENQUEUE_TIME" bigint;
 
+-- HIVE-20221
+alter table "PARTITION_PARAMS" alter column "PARAM_VALUE" type text using cast("PARAM_VALUE" as text);
+
 -- These lines need to be last.  Insert any changes above.
 UPDATE "VERSION" SET "SCHEMA_VERSION"='3.1.0', "VERSION_COMMENT"='Hive release version 3.1.0' where "VER_ID"=1;
 SELECT 'Finished upgrading MetaStore schema from 3.0.0 to 3.1.0';
